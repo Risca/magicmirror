97c385d7dbb0d15b51b3026176cbf4eda17cd929
Patrik Dahlström <risca@powerlamerz.org>
2020-11-16 00:39:59 +0100

    calendar: make event summary pop out more

--- widgets/calendar/calendar.cpp
+++ widgets/calendar/calendar.cpp
@@ -7,6 +7,7 @@
 #include "utils/effects.h"
 #include "utils/settingsfactory.h"
 
+#include <QApplication>
 #include <QBrush>
 #include <QDebug>
 #include <QFontMetrics>
@@ -14,14 +15,83 @@
 #include <QGraphicsOpacityEffect>
 #include <QLinearGradient>
 #include <QLocale>
+#include <QModelIndex>
 #include <QNetworkAccessManager>
 #include <QPainter>
 #include <QPixmap>
 #include <QSettings>
+#include <QStyle>
+#include <QStyledItemDelegate>
 #include <QTextCharFormat>
 #include <QUrl>
 
 namespace calendar {
+
+namespace {
+
+class EventItemDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    EventItemDelegate(QObject *parent) : QStyledItemDelegate(parent) { }
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &vopt, const QModelIndex &index) const
+    {
+#if QT_VERSION >= 0x050E00
+        QStringList texts = index.data().toString().split(' ', Qt::SkipEmptyParts);
+#else
+        QStringList texts = index.data().toString().split(' ', QString::SkipEmptyParts);
+#endif
+        if (texts.size() < 2) {
+            painter->restore();
+            return QStyledItemDelegate::paint(painter, vopt, index);
+        }
+        const QString &date = texts.takeFirst();
+        const QIcon icon = index.data(Qt::DecorationRole).value<QIcon>();
+        const QString summary = texts.join(' ');
+
+        const QWidget *widget = vopt.widget;
+        QStyle *style = widget ? widget->style() : QApplication::style();
+
+        painter->save();
+        painter->setClipRect(vopt.rect);
+
+        // Draw the background
+        style->drawPrimitive(QStyle::PE_PanelItemViewItem, &vopt, painter, widget);
+
+        // Draw the date
+        painter->save();
+        // Reduce the font a bit
+        QFont font = painter->font();
+        font.setPointSize(font.pointSize() * 2 / 3);
+        painter->setFont(font);
+        // Write date with darker colors
+        painter->setPen(vopt.palette.color(QPalette::Disabled, QPalette::Text));
+        QFontMetrics fm = painter->fontMetrics();
+#if QT_VERSION >= 0x050B00
+        int width = fm.horizontalAdvance(date);
+#else
+        int width = fm.width(date);
+#endif
+        QRect textRect = QRect(vopt.rect.topLeft(), QSize(width, vopt.rect.height()));
+        painter->drawText(textRect, vopt.displayAlignment, date);
+        painter->restore();
+
+        // Draw the icon
+        QRect iconRect = QRect(textRect.topRight(), QSize(1, 1) * vopt.rect.height());
+        icon.paint(painter, iconRect, Qt::AlignHCenter | Qt::AlignVCenter);
+
+        // Draw the summary
+        fm = painter->fontMetrics();
+        textRect = QRect(iconRect.topRight(), vopt.rect.bottomRight() + QPoint(1, 1));
+        painter->drawText(textRect, vopt.displayAlignment, summary);
+
+        painter->restore();
+    }
+};
+
+}
 
 #define CALENDAR_SYNC_PERIOD (2 * 60 * 60 * 1000)
 
@@ -47,6 +117,7 @@ Calendar::Calendar(ISource *dataSource, QWidget *parent)
     ui->setupUi(this);
 
     utils::ApplyFade(ui->events);
+    ui->events->setItemDelegate(new EventItemDelegate(ui->events));
 
     ui->calendar->setFirstDayOfWeek(locale().firstDayOfWeek());
 
@@ -103,3 +174,5 @@ void Calendar::NewEventList(const QList<Event> &events)
 }
 
 }
+
+#include "calendar.moc"
