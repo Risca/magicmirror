424f31ef3dc421485dd5cd3d426f967e5444eee9
Patrik Dahlström <risca@powerlamerz.org>
2021-05-29 16:13:47 +0200

    main: whitespace

--- main/main.cpp
+++ main/main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char **argv)
     if (!f.exists()) {
         qWarning() << "Unable to set stylesheet, file not found";
     }
-    else   {
+    else {
         f.open(QFile::ReadOnly | QFile::Text);
         QTextStream ts(&f);
         qApp->setStyleSheet(ts.readAll());
