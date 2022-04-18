510dd2ba66b2568838764da65c6709169b4c7783
Patrik Dahlström <risca@powerlamerz.org>
2022-04-18 19:49:51 +0200

    slideshow: switch to QImageReader for reading images
    It has better performance and can also rotate images based on EXIF (I
    assume)

--- widgets/slideshow.cpp
+++ widgets/slideshow.cpp
@@ -2,8 +2,12 @@
 
 #include "utils/settingsfactory.h"
 
+#include <QDebug>
 #include <QDir>
+#include <QImageReader>
 #include <QPixmap>
+#include <QResizeEvent>
+#include <QSizePolicy>
 
 #define SLIDESHOW_DEFAULT_INTERVAL (10)
 
@@ -20,12 +24,48 @@ bool Slideshow::Create(Slideshow *&slideshow, QWidget *parent)
             foreach (const QString& file, files) {
                 paths << dir.absoluteFilePath(file);
             }
-            int interval = settings->value("interval", SLIDESHOW_DEFAULT_INTERVAL).toInt() * 1000;
-            slideshow = new Slideshow(paths, interval, parent);
+            int interval = settings->value("interval", SLIDESHOW_DEFAULT_INTERVAL).toInt();
+            qDebug() << __PRETTY_FUNCTION__ << "Found" << paths.size() << "images to show, switching every" << interval << "second";
+            slideshow = new Slideshow(paths, interval * 1000, parent);
             return true;
         }
     }
     return false;
+}
+
+QSize Slideshow::minimumSizeHint() const
+{
+    return QSize(20, 20);
+}
+
+QSize Slideshow::sizeHint() const
+{
+    int w = this->width();
+    return QSize(w, heightForWidth(w));
+}
+
+void Slideshow::resizeEvent(QResizeEvent *e)
+{
+    if (!m_lastFile.isEmpty()) {
+        QImageReader img;
+        img.setAutoTransform(true);
+        img.setFileName(m_lastFile);
+        if (img.canRead()) {
+            QSize const imgSize = img.size();
+            QSize scaledSize;
+
+            if (img.transformation() & QImageIOHandler::TransformationRotate90) {
+                QSize const realImgSize = imgSize.transposed();
+                scaledSize = realImgSize.scaled(QSize(e->size().width(), realImgSize.height()), Qt::KeepAspectRatio).transposed();
+            }
+            else {
+                scaledSize = imgSize.scaled(QSize(e->size().width(), imgSize.height()), Qt::KeepAspectRatio);
+            }
+
+            img.setScaledSize(scaledSize);
+            setPixmap(QPixmap::fromImageReader(&img));
+        }
+    }
 }
 
 Slideshow::Slideshow(const QStringList &files, int interval, QWidget *parent) :
@@ -36,16 +76,46 @@ Slideshow::Slideshow(const QStringList &files, int interval, QWidget *parent) :
     this->fadeOut();
 }
 
+int Slideshow::heightForWidth(int width) const
+{
+#if (QT_VERSION < QT_VERSION_CHECK(5, 15, 0))
+    const QPixmap *pix = this->pixmap();
+    return !pix ? this->height() : ((qreal)pix->height()*width)/pix->width();
+#else
+    const QPixmap& pix = this->pixmap(Qt::ReturnByValue);
+    return pix.isNull() ? this->height() : ((qreal)pix.height()*width)/pix.width();
+#endif
+}
+
 void Slideshow::changePicture()
 {
-    int idx, retries = 10;
-    QPixmap pic;
+    const int numFiles = m_files.size();
+    int retries = 10;
+    QImageReader img;
+
+    img.setAutoTransform(true);
 
     do {
-        idx = rand() % m_files.size();
-    } while (retries-- && !pic.load(m_files[idx]));
+        const QString& filename = m_files[rand() % numFiles];
+        if (filename == m_lastFile)
+            continue;
+        m_lastFile = filename;
+        img.setFileName(filename);
+    } while (retries-- && !img.canRead());
 
-    if (!pic.isNull()) {
-        setPixmap(pic.scaledToWidth(width()));
+    if (img.canRead()) {
+        // Don't actually read the image file here, just resize the widget and read the file in resizeEvent()
+        QSize const imgSize = img.transformation() & QImageIOHandler::TransformationRotate90 ?
+                    img.size().transposed() : img.size();
+        QSize const scaledSize = imgSize.scaled(QSize(width(), imgSize.height()), Qt::KeepAspectRatio);
+
+        if (scaledSize == this->size()) {
+            // If the size doesn't change, no event will get posted, unless we do it.
+            QResizeEvent *e = new QResizeEvent(scaledSize, this->size());
+            QCoreApplication::postEvent(this, e);
+        }
+        else {
+            this->resize(scaledSize);
+        }
     }
 }
--- widgets/slideshow.h
+++ widgets/slideshow.h
@@ -10,11 +10,19 @@ class Slideshow : public FadingQLabel
 public:
     static bool Create(Slideshow*& slideshow, QWidget *parent = 0);
 
+    virtual QSize minimumSizeHint() const;
+    virtual QSize sizeHint() const;
+
+    virtual void resizeEvent(QResizeEvent* e);
+
 public slots:
     void changePicture();
 
 private:
     Slideshow(const QStringList &files, int interval, QWidget *parent = 0);
 
+    int heightForWidth(int width) const;
+
     const QStringList m_files;
+    QString m_lastFile;
 };
